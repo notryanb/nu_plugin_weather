d99f294be2b952a9b1913f356cd758a41178a1e1
Ryan Blecher <notryanb@gmail.com>
2020-04-05 15:17:29 -0400

    add readme - forgot to commit city code

---
+++ src/README.md
@@ -0,0 +1,28 @@
+# nu_plugin_weather
+
+This is currently a _very experimental_ project for wrapping the OpenWeatherAPI in a nu plugin.
+The default location is Huntington, NY, however a city can be passed in with `--city` or `-c`.
+The city must currently have no spaces... because I have not yet safely encoded the URL query params.
+
+The raw JSON is returned and turned into a table. 
+Eventually this will be mapped to much nicer data displaying emojis.
+
+## Setup (currently only build from source)
+
+- From the build directory, `cargo install --path .`
+- You must have an API Key from the [OpenWeather API](https://openweathermap.org/api)
+- It must be saved as an environment variable on your machine as `OPEN_WEATHER_API_KEY`
+
+## Usage
+
+- `weather`
+- `weather --city philadelphia`
+
+## Ideas & Goals
+
+- Have good top level information for 7 day forecast, hourly forecast, and current forecast.
+- Support emojis for weather display
+- Optional temperature scales [ Fahrenheit, Celcius, Kelvin ]
+- Easier to read date time (conversions from Unix epoch). Timezones!
+- Support for more countries than US (default scales based on country?)
+- Better structure for code - see supported nu plugins.
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,37 @@ use futures::executor::block_on;
 use nu_errors::ShellError;
 use nu_plugin::{serve_plugin, Plugin};
 use nu_protocol::{
-    CallInfo, CommandAction, ReturnSuccess, ReturnValue, Signature, UntaggedValue, Value,
+    CallInfo, CommandAction, ReturnSuccess, ReturnValue, Signature, SyntaxShape, UntaggedValue, Value,
 };
 use nu_source::{AnchorLocation, Span, Tag};
 
 struct Weather {
     pub api_key: String,
+    pub city: Option<String>,
 }
 
 impl Plugin for Weather {
     fn config(&mut self) -> Result<Signature, ShellError> {
         Ok(Signature::build("weather")
             .desc("Displays weather information")
+            .named(
+                "city",
+                SyntaxShape::Any,
+                "the city to retrieve weather for",
+                Some('c'),
+            )
             .filter())
     }
 
     fn begin_filter(&mut self, call_info: CallInfo) -> Result<Vec<ReturnValue>, ShellError> {
+        self.city = match call_info.args.get("city") {
+            Some(city) => Some(city.as_string()?),
+            None => Some("huntington".to_string()),
+        };
+
         let url = format!(
-            "https://api.openweathermap.org/data/2.5/forecast?&mode=json&q=huntington&appid={}",
+            "https://api.openweathermap.org/data/2.5/forecast?&mode=json&q={}&appid={}",
+            self.city.as_ref().unwrap(),
             self.api_key
         );
         Ok(vec![block_on(weather_helper(&url, &call_info.name_tag))])
@@ -38,7 +51,7 @@ fn main() {
     let api_key = std::env::var("OPEN_WEATHER_API_KEY")
         .expect("Missing OPEN_WEATHER_API_KEY ENV var")
         .to_string();
-    serve_plugin(&mut Weather { api_key });
+    serve_plugin(&mut Weather { api_key, city: None });
 }
 
 pub async fn weather_helper(url: &str, tag: &Tag) -> ReturnValue {
